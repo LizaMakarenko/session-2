7b0b4dcca8a1547adb1802f3edb971fe2a33bb57
Elizaveta Makarenko <lizzamakmakarenko@yandex.ru>
2025-06-30 15:32:27 +0300

    data preparation and formatting done

--- src/main/java/ru/sbt/jschool/session2/OutputFormatter.java
+++ src/main/java/ru/sbt/jschool/session2/OutputFormatter.java
@@ -18,6 +18,9 @@
 package ru.sbt.jschool.session2;
 
 import java.io.PrintStream;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  */
@@ -25,10 +28,42 @@ public class OutputFormatter {
     private PrintStream out;
 
     public OutputFormatter(PrintStream out) {
+
         this.out = out;
     }
 
     public void output(String[] names, Object[][] data) {
-        //TODO: implement me.
+        int columns = names.length;
+        int [] columnWidths = new int[columns];
+        String [] [] table = new String[data.length][columns];
+        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
+        DecimalFormat moneyf = new DecimalFormat("#,##0.00");
+        DecimalFormat  numberf = new DecimalFormat("#,###");
+
+        for (int i=0; i<columns; i++) {
+            columnWidths[i] = table[i][0].length();
+        }
+        for (int row=0; row<data.length; row++) {
+            for(int col=0; col<columns; col++) {
+                Object value = data[row][col];
+                String text;
+                if (value == null) {
+                    text = "-";
+                } else if (value instanceof String) {
+                    text = ((String) value).replace("\n", " ");
+                } else if (value instanceof Date) {
+                    text = sdf.format((Date) value);
+                } else if (value instanceof Float || value instanceof Double) {
+                    text = moneyf.format(((Number) value).doubleValue());
+                } else if (value instanceof Number) {
+                    text = numberf.format(((Number) value).longValue());
+                } else {
+                    text = value.toString();
+                }
+                table[row][col]=text;
+                columnWidths[col]=Math.max(columnWidths[col], text.length());
+            }
+        }
+
     }
 }
